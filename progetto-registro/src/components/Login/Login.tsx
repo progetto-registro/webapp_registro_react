f9c868d5c3bf7f8ffe8911a74f1532ec02cbf971
rinaka1504 <rnafsir@gmail.com>
2025-09-15 14:48:56 +0200

    commit

--- progetto-registro/src/components/Login/Login.tsx
+++ progetto-registro/src/components/Login/Login.tsx
@@ -1,48 +1,52 @@
-import Button from '@mui/material/Button';
 import { useState } from 'react';
+import Button from '@mui/material/Button';
+import './Login.css';
 
 export default function Login() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState<string>('');  
+  const [password, setPassword] = useState<string>('');   
+
   const handleLogin = () => {
     console.log("Username:", username, "Password:", password);
-  }
+  };
 
   return (
-    <>
-      <div className='login-container'>
+    <div className="login-container">
+      <div>
         <div>
-          <div>
-            <label htmlFor="username">Username:</label>
-          </div>
-          <input 
-            type="text"
-            id="username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            placeholder='Inserisci username...'
-            autoComplete='username'
-          />
+          <label htmlFor="username">Username</label>
         </div>
-
-        <div>
-          <div>
-            <label htmlFor="password">Password:</label>
-          </div>
-          <input 
-            type="password"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            placeholder='Inserisci password...'
-            autoComplete='password'
-          />
-        </div>
-
-        <Button variant="contained" onClick={handleLogin}>
-          Login
-        </Button>
+        <input 
+          type="text"
+          id="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          placeholder="Inserisci username..."
+          autoComplete="username"
+        />
       </div>
-    </>
+
+      <div>
+        <div>
+          <label htmlFor="password">Password</label>
+        </div>
+        <input 
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Inserisci password..."
+          autoComplete="current-password"
+        />
+      </div>
+
+      <Button 
+        variant="contained" 
+        onClick={handleLogin} 
+        className="button"
+      >
+        Login
+      </Button>
+    </div>
   );
 }
