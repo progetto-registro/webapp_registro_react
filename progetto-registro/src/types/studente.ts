eab5effb48c1536e1975913709cea2355e712041
FrankFTP <francesco.airenti.14-1@accademiadigitaleliguria.it>
2025-09-17 16:03:52 +0200

    modifica studente.ts

--- progetto-registro/src/types/studente.ts
+++ progetto-registro/src/types/studente.ts
@@ -1,9 +1,12 @@
+import dayjs from "dayjs";
+
+
 export type Studente = {
     nome : string;
     cognome : string;
     dataNascita : string;
     sesso : string;
-    codiceFiscale : string;
+    codiceFiscale : dayjs.Dayjs | string;
     email : string;
     password : string;
 
