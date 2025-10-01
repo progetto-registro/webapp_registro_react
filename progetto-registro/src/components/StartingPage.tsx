336ee38cb007c82ee271c26e0f4f058c05e3d556
rinaka1504 <rnafsir@gmail.com>
2025-10-01 18:32:37 +0200

    css

--- progetto-registro/src/components/StartingPage.tsx
+++ progetto-registro/src/components/StartingPage.tsx
@@ -1,12 +1,19 @@
-import { Button, Stack } from "@mui/material";
+import { Button, Stack, Typography, Box } from "@mui/material";
 import { Link } from "react-router";
 
 export function StartingPage() {
 
     return (
-        <Stack direction="row" spacing={2}>
-            <Button variant="contained" component={Link} to="/login">Login</Button>
-            <Button variant="contained" component={Link} to="/signup">Registrati</Button>
-        </Stack>
+        <>
+        <Typography variant="h3" textAlign="center" fontWeight="bold">
+            Accedi a Registrum</Typography>
+
+        <Box textAlign="center" mt={4} >
+            <Stack direction="row" spacing={2} justifyContent="center">
+                <Button variant="contained" fullWidth component={Link} to="/login">Login</Button>
+                <Button variant="contained" fullWidth component={Link} to="/signup">Registrati</Button>
+            </Stack>
+        </Box>
+        </>
     );
 }
